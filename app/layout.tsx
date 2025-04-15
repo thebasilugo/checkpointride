0b455e412f5fa13d3c13c763a26ddafb9f688b67
Basil Ugo <basilugo2@gmail.com>
2025-04-15 12:40:31 +0100

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-	title: "thebasilugo App",
-	description: "Created with thebasilugo",
-	generator: "thebasilugo.dev",
+	title: "CheckpointRide",
+	description: "Ride hailing application that uses checkpoints",
+	generator: "thebasilugo",
 };
 
 export default function RootLayout({
